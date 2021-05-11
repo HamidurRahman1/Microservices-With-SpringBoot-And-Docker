60f697db703d9f2a5d169ab5869ab08506b39b40
Hamidur Rahman <Hamidur0405@gmail.com>
2021-05-11 01:22:34 -0400

    resource folder was in wrong path, resolved now

---
+++ CollegeAPI/src/main/resources/schema.sql
@@ -0,0 +1,16 @@
+drop schema if exists central;
+
+create schema if not exists central;
+
+use central;
+
+create table colleges
+(college_id int primary key auto_increment,
+short_name varchar(5) not null unique,
+full_name varchar(50) not null unique,
+join_date date not null);
+
+create table courses
+(course_id int primary key auto_increment,
+catalog_name varchar(5) not null unique,
+description varchar(500) not null unique);
